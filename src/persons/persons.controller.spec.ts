d359de0fa994f1758af28c972b2599270f9e4473
nikitakocherinskiy <nik.koch.67@yandex.ru>
2023-10-19 02:22:03 +0300

    fix: unit and e2e tests

--- src/persons/persons.controller.spec.ts
+++ src/persons/persons.controller.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PersonsController } from './persons.controller';
 import { PersonsService } from './persons.service';
+import { DatabaseService } from 'src/database/database.service';
+import { PersonsModule } from './persons.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 describe('PersonsController', () => {
   let controller: PersonsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PersonsModule, DatabaseModule],
       controllers: [PersonsController],
-      providers: [PersonsService],
-      exports: [PersonsService],
+      providers: [PersonsService, DatabaseService],
     }).compile();
 
     controller = module.get<PersonsController>(PersonsController);
--- src/persons/persons.service.spec.ts
+++ src/persons/persons.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PersonsService } from './persons.service';
+import { PersonsModule } from './persons.module';
+import { DatabaseModule } from 'src/database/database.module';
+import { PersonsController } from './persons.controller';
 
 describe('PersonsService', () => {
   let service: PersonsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PersonsService],
+      imports: [PersonsModule, DatabaseModule],
+      controllers: [PersonsController],
+      providers: [PersonsService, DatabaseModule],
     }).compile();
 
     service = module.get<PersonsService>(PersonsService);
